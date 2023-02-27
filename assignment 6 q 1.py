14fac9aba4ccd55fa2d94718c8f859bcca8ebd18
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-02-26 21:24:37 -0500

    Add files via upload

---
+++ assignment 6 q 1.py
@@ -0,0 +1,33 @@
+lower = input('input lower bound')
+upper = input('input upper bound')
+stepsize = input('input step size for the group')
+
+#insert a loop for program
+while(input(-1:10)):
+#there should only be 3 inputted values
+    if(lower == 0):
+        print('you have to input values for all variables - 3')
+
+    if(upper == 0):
+        print('you have to input values for all variables - 3')
+
+    if(stepsize == 0):
+        print('you have to input values for all variables - 3')
+
+
+sort = print(lower, stepsize, upper)
+
+mean = lower * upper * stepsize / 3 
+print(mean) 
+
+if(stepsize>lower):
+    if(stepsize<upper):
+        stepsize = median
+print(median)
+
+rang = upper - lower 
+print(rang)
+
+break
+
+
