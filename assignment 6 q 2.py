b98b8b5f1d0922fc24cdf32123824b6a13bbf306
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-02-27 19:45:39 -0500

    Add files via upload

---
+++ assignment 6 q 2.py
@@ -0,0 +1,23 @@
+message = input
+#convert to octal
+for(i == 0, message>0, i):
+    octal[i] = message % 8
+          message = message / 8
+
+print('octal number')
+for(j=i-1 ,j>=0 ,j):
+    print(octal[j])
+
+#if it cant be converted to octal, stays a negative decimal
+if(input != %8)
+    else(input != /8)
+#it stays a negative integer
+input = message
+print(message)
+
+#convert to ascii
+for(input):
+    ascii(octal)
+    repr(message)
+    print('the message is ascii is')
+    print(ascii(octal))
